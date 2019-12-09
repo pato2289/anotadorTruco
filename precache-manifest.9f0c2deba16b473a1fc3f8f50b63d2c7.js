f232e02232b7180d9b33ca3bfe8279bd17d389d0
patobordenave <patriciofbordenave@gmail.com>
2019-12-09 18:16:00 -0300

    Updates

---
+++ precache-manifest.9f0c2deba16b473a1fc3f8f50b63d2c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ceef8fa8e835d3a17f201f9436bc899",
+    "url": "/anotadorTruco/index.html"
+  },
+  {
+    "revision": "702c4ed1e43f83d5c6f8",
+    "url": "/anotadorTruco/static/css/main.9232d9dd.chunk.css"
+  },
+  {
+    "revision": "3fce0449f1bdd2222242",
+    "url": "/anotadorTruco/static/js/2.393d467d.chunk.js"
+  },
+  {
+    "revision": "702c4ed1e43f83d5c6f8",
+    "url": "/anotadorTruco/static/js/main.ad604fe5.chunk.js"
+  },
+  {
+    "revision": "38dcf7c7f1356ef77c83",
+    "url": "/anotadorTruco/static/js/runtime-main.f619581f.js"
+  }
+]);
